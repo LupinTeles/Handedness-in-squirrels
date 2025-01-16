87aa14a5d81e17bcaff3ab378cbcb58d03016647
Lupin Teles <lupin06@outlook.com>
2025-01-16 17:32:37 -0300

    Upload os CSV file with selected columns

--- Code.R
+++ Code.R
@@ -3,3 +3,9 @@ data<- read.csv("CombinedCSV.csv", header= TRUE, stringsAsFactors=FALSE)
 
 #View data
 str(data)
+
+#Upload CSV with selected columns
+Variables<- read.csv("SelectedColumns.csv", header=TRUE, stringsAsFactors = FALSE)
+
+#View data
+str(Variables)
